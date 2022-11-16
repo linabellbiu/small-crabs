9eacf3820fe677f3767969f1349bd9132d0f3151
wangxd01 <wangxd01@mingyuanyun.com>
2022-11-17 00:14:09 +0800

    输出标准错误

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use small_crabs::{Config, run};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("输入的参数错误: {}", err);
+        eprintln!("输入的参数错误: {}", err);
         process::exit(1);
     });
 
@@ -13,7 +13,7 @@ fn main() {
     // println!("在这个文件:{}", config.filename);
 
     if let Err(err) = run(config) {
-        println!("发生错误: {}", err);
+        eprintln!("发生错误: {}", err);
         process::exit(1);
     }
 }
