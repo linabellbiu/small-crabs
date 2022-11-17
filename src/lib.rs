a1d9b4efb67aeda3b5c325014abfc6cea52fdbef
wangxd01 <wangxd01@mingyuanyun.com>
2022-11-17 17:27:01 +0800

    去掉clone

--- src/lib.rs
+++ src/lib.rs
@@ -51,26 +51,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines()
+        .filter(|p| p.contains(query))
+        .collect()
 }
 
 pub fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    let query = query.to_lowercase();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines()
+        .filter(|p| { p.to_lowercase().contains(&query.to_lowercase()) })
+        .collect()
 }
 
 
